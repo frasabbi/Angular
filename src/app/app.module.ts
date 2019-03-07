89cf8c1cdc768a01d6d361a3177a13a6a45e68ca
Nicoletta Magi <45179337+nikym91@users.noreply.github.com>
2019-03-07 12:32:21 +0100

    masterpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MaxPlayersComponent } from './max-players/max-players.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MasterPageComponent } from './master-page/master-page.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      MaxPlayersComponent
+      MaxPlayersComponent,
+      WelcomeComponent,
+      MasterPageComponent,
+     
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      FormsModule
+      FormsModule,
+      RouterModule.forRoot([
+         { path: 'welcome', component: WelcomeComponent },
+         { path: 'courts', component: AppComponent },
+         { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+         { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+       ]),
    ],
    providers: [],
    bootstrap: [
-      AppComponent
+      MasterPageComponent
    ]
 })
 export class AppModule { }
